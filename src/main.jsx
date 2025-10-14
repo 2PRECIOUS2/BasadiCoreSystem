fbc7c1029d87623912dcfe33e16eebd6678a0aff
Ntokozo <113803719+2PRECIOUS2@users.noreply.github.com>
2025-10-14 14:19:03 +0200

    Remove basename from BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Suspense>
-    <BrowserRouter basename='/Basadi-Core'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Suspense>,
