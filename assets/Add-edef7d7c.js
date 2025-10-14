cddcfa38804811fc4796a0dd3ba5689fa32af922
Ntokozo <ntokozo.kubone04@gmail.com>
2025-10-14 13:05:23 +0200

    Updates

---
+++ assets/Add-edef7d7c.js
@@ -0,0 +1,1 @@
+import{a0 as o,k as s}from"./index-ed8ecba8.js";const a=o(s.jsx("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"}),"Add");export{a as A};
---
+++ assets/BlankLayout-55912eea.js
@@ -0,0 +1,1 @@
+import{k as t,Z as s}from"./index-ed8ecba8.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})});export{e as default};
