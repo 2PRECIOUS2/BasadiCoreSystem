0d4eb4fcb8b8cce7032f6d5bd403052727c0d30b
Ntokozo <113803719+2PRECIOUS2@users.noreply.github.com>
2025-10-17 01:06:26 +0200

    Refactor vite.config.js for improved configuration

--- vite.config.js
+++ vite.config.js
@@ -1,67 +1,62 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import fs from 'fs/promises';
 import svgr from '@svgr/rollup';
-// import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  // Load API URL from env
+  const API_URL = process.env.VITE_API_URL || 'http://localhost:5000';
+
+  return {
     resolve: {
-        alias: {
-            src: resolve(__dirname, 'src'),
-        },
+      alias: {
+        src: resolve(__dirname, 'src'),
+      },
     },
     assetsInclude: ['**/*.PNG'],
     esbuild: {
-        loader: 'jsx',
-        include: /src\/.*\.jsx?$/,
-        exclude: [],
+      loader: 'jsx',
+      include: /src\/.*\.jsx?$/,
+      exclude: [],
     },
-
-      server: {
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  },
+    server: {
+      proxy: mode === 'development' ? { '/api': 'http://localhost:5000' } : undefined,
+    },
     optimizeDeps: {
-        esbuildOptions: {
-            plugins: [
-                {
-                    name: 'load-js-files-as-jsx',
-                    setup(build) {
-                        build.onLoad(
-                            { filter: /src\\.*\.js$/ },
-                            async (args) => ({
-                                loader: 'jsx',
-                                contents: await fs.readFile(args.path, 'utf8'),
-                            })
-                        );
-                    },
-                },
-            ],
-        },
+      esbuildOptions: {
+        plugins: [
+          {
+            name: 'load-js-files-as-jsx',
+            setup(build) {
+              build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
+                loader: 'jsx',
+                contents: await fs.readFile(args.path, 'utf8'),
+              }));
+            },
+          },
+        ],
+      },
     },
-
-    
-    // plugins: [react(),svgr({
-    //   exportAsDefault: true
-    // })],
-
     plugins: [svgr(), react()],
     base: './',
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(API_URL), // make available in frontend code
+    },
     build: {
-        outDir: 'build',
-        sourcemap: false,
-        chunkSizeWarningLimit: 2400,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['react', 'react-dom'],
-                    mui: ['@mui/material', '@mui/icons-material'],
-                    utils: ['lodash', 'axios']
-                }
-            }
-        }
-    }
+      outDir: 'build',
+      sourcemap: false,
+      chunkSizeWarningLimit: 2400,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+            mui: ['@mui/material', '@mui/icons-material'],
+            utils: ['lodash', 'axios'],
+          },
+        },
+      },
+    },
+  };
 });
